c14038b2eedc803e80a1889f1936b4c74cb76741
Jhsysng <ysng1118@naver.com>
2025-01-07 18:31:11 +0900

    initial commit

---
+++ src/main/java/info/reinputapigateway/ReinputApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package info.reinputapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReinputApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReinputApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/reinputapigateway/ReinputApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package info.reinputapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReinputApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
