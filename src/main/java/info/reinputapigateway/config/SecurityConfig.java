fcbd566f0784c6d5ce98567d2cd40e32eccfeed6
Jhsysng <ysng1118@naver.com>
2025-01-22 14:50:15 +0900

    RE-168 security 설정 추가

---
+++ src/main/java/info/reinputapigateway/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package info.reinputapigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/*/swagger-ui/**",
+                                "/*/swagger-ui.html",
+                                "/*/v3/api-docs/**",
+                                "/*/swagger-resources/**",
+                                "/auth/**",
+                                "/oauth2/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
