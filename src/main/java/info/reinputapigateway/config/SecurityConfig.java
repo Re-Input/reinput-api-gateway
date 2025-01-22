16a1d88d9dad804b5b1f3ba81131d9c41d589e36
Jhsysng <ysng1118@naver.com>
2025-01-22 15:32:50 +0900

    RE-168 chore: security config webflux 의존 추가

--- src/main/java/info/reinputapigateway/config/SecurityConfig.java
+++ src/main/java/info/reinputapigateway/config/SecurityConfig.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
